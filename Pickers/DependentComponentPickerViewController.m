964c19702da5ad15e025d8852ea2ec31c74bc1dc
Leo Peng <pengzimaobuaa@163.com>
2016-06-05 11:31:19 +0800

    Add dependent picker

--- Pickers/DependentComponentPickerViewController.m
+++ Pickers/DependentComponentPickerViewController.m
@@ -8,7 +8,15 @@
 
 #import "DependentComponentPickerViewController.h"
 
+#define kStateComponent 0
+#define kZipComponent 1
+
 @interface DependentComponentPickerViewController ()
+
+@property (strong, nonatomic) NSDictionary *stateZips;
+@property (strong, nonatomic) NSArray *states;
+@property (strong, nonatomic) NSArray *zips;
+@property (weak, nonatomic) IBOutlet UIPickerView *dependentPicker;
 
 @end
 
@@ -17,6 +25,17 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    NSBundle *bundle = [NSBundle mainBundle];
+    NSURL *plistURL = [bundle URLForResource:@"statedictionary" withExtension:@"plist"];
+    
+    self.stateZips = [NSDictionary dictionaryWithContentsOfURL:plistURL];
+    
+    NSArray *allStates = [self.stateZips allKeys];
+    NSArray *sortedStates = [allStates sortedArrayUsingSelector:@selector(compare:)];
+    self.states = sortedStates;
+    
+    NSString *selectedState = self.states[0];
+    self.zips = self.stateZips[selectedState];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -33,5 +52,50 @@
     // Pass the selected object to the new view controller.
 }
 */
+- (IBAction)buttonPressed:(UIButton *)sender {
+    NSInteger stateRow = [self.dependentPicker selectedRowInComponent:kStateComponent];
+    NSInteger zipRow = [self.dependentPicker selectedRowInComponent:kZipComponent];
+    
+    NSString *state = self.states[stateRow];
+    NSString *zip = self.zips[zipRow];
+    
+    NSString *title = [[NSString alloc] initWithFormat:@"You selected zip code %@", zip];
+    NSString *message = [[NSString alloc] initWithFormat:@"%@ is in %@", zip, state];
+    
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:title message:message preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction *action = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:nil];
+    [alert addAction:action];
+    [self presentViewController:alert animated:YES completion:nil];
+}
 
+#pragma mark - 
+#pragma mark Picker Data Source Methods
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView {
+    return 2;
+}
+
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component {
+    if (component == kStateComponent) {
+        return [self.states count];
+    } else {
+        return [self.zips count];
+    }
+}
+
+- (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component {
+    if (component == kStateComponent) {
+        return self.states[row];
+    } else {
+        return self.zips[row];
+    }
+}
+
+- (void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component {
+    if (component == kStateComponent) {
+        NSString *selectedState = self.states[row];
+        self.zips = self.stateZips[selectedState];
+        [self.dependentPicker reloadComponent:kZipComponent];
+        [self.dependentPicker selectRow:0 inComponent:kZipComponent animated:YES];
+    }
+}
 @end
